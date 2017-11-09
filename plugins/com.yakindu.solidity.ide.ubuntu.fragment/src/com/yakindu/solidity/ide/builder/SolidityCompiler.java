2b98f41dda69e9f83353fcfc896149b4bbe58d38
Florian Antony <florian.antony@itemis.de>
2017-11-09 11:15:01 +0100

    rename fragment

--- plugins/com.yakindu.solidity.ide.ubuntu.fragment/src/com/yakindu/solidity/ide/builder/SolidityCompiler.java
+++
@@ -1,9 +0,0 @@
-package com.yakindu.solidity.ide.builder;
-
-public class SolidityCompiler extends SolidityCompilerBase {
-
-	@Override
-	protected String getCompiler() {
-		return getClass().getProtectionDomain().getCodeSource().getLocation().getFile() + "/compiler/solc";
-	}
-}
