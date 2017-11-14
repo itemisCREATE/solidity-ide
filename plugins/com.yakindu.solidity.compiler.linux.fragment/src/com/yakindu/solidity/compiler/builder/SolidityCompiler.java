76b22ae894c53e0db09d649ce07cfc7c44201d38
Florian Antony <florian.antony@itemis.de>
2017-11-14 20:30:49 +0100

    add new fragments

---
+++ plugins/com.yakindu.solidity.compiler.linux.fragment/src/com/yakindu/solidity/compiler/builder/SolidityCompiler.java
@@ -0,0 +1,11 @@
+package com.yakindu.solidity.compiler.builder;
+
+import com.yakindu.solidity.compiler.builder.SolidityCompilerBase;
+
+public class SolidityCompiler extends SolidityCompilerBase {
+
+	@Override
+	protected String getCompiler() {
+		return getClass().getProtectionDomain().getCodeSource().getLocation().getFile() + "/compiler/solc";
+	}
+}
