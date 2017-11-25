3c5badfee2eeeef03ea87ffdb0f1381a2740bf0e
Karsten Thoms <karsten.thoms@itemis.de>
2017-11-26 00:33:45 +0100

    Added test for reference examples
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ plugins/com.yakindu.solidity.tests/src/com/yakindu/solidity/tests/ReferenceExamplesTest.java
@@ -0,0 +1,53 @@
+package com.yakindu.solidity.tests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.eclipse.xtext.testing.validation.ValidationTestHelper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.yakindu.solidity.solidity.SolidityModel;
+
+/**
+ * This test reads all .sol files from project com.yakindu.solidity.examples
+ * and parses and validates them.
+ * 
+ * @author Karsten Thoms
+ */
+@ExtendWith(InjectionExtension.class)
+@InjectWith(SolidityInjectorProvider.class)
+public class ReferenceExamplesTest {
+	@Inject
+	ParseHelper<SolidityModel> parseHelper;
+	@Inject
+	ValidationTestHelper validationHelper;
+
+	@ParameterizedTest
+	@MethodSource("getReferenceFiles")
+	public void test_parseReferenceFile(Path path) throws Exception {
+		String content = new String(Files.readAllBytes(path));
+		SolidityModel model = parseHelper.parse(content);
+		validationHelper.assertNoErrors(model);
+	}
+
+	@SuppressWarnings("unused")
+	private static Stream<Path> getReferenceFiles() throws IOException {
+		Path baseDir = Paths.get("../../examples/com.yakindu.solidity.examples");
+		assertTrue(baseDir.toFile().exists());
+		return Files.walk(baseDir, new FileVisitOption[0])
+				.filter(f -> f.toString().endsWith(".sol"));
+	}
+}
