ab13bcb61a08d438993d2020387d6c92ffc6095f
Florian Antony <florian.antony@itemis.de>
2018-04-26 13:46:22 +0200

    Create remix like coloring

--- plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/highlighting/SolidityHighlightingConfiguration.java
+++ plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/highlighting/SolidityHighlightingConfiguration.java
@@ -17,6 +17,7 @@ package com.yakindu.solidity.ui.highlighting;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
 
 /**
@@ -26,11 +27,61 @@ import org.eclipse.xtext.ui.editor.utils.TextStyle;
  */
 public class SolidityHighlightingConfiguration extends DefaultHighlightingConfiguration {
 
+	public static final String VERSION = "version";
+	public static final String TYPES = "types";
+	public static final String PARAMETER = "parameter";
+	public static final String FUNCTION_NAME = "function_name";
+	public static final String FEATURESCALL = "feature";
+	
+	
+	@Override
+	public void configure(IHighlightingConfigurationAcceptor acceptor) {
+		super.configure(acceptor);
+		acceptor.acceptDefaultHighlighting(VERSION, "VariableDefinition", solidityVersionTextStyle());
+		acceptor.acceptDefaultHighlighting(TYPES, "VariableDefinition", typeTextStyle());
+		acceptor.acceptDefaultHighlighting(PARAMETER, "VariableDefinition", typeTextStyle());
+		acceptor.acceptDefaultHighlighting(FUNCTION_NAME, "VariableDefinition", functionNameTextStyle());
+		acceptor.acceptDefaultHighlighting(FEATURESCALL, "VariableDefinition", featureCallTextStyle());
+		
+	}
+	
+	private TextStyle typeTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(49, 132, 149));
+		return textStyle;
+	}
+	
+	private TextStyle functionNameTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(0, 0, 162));
+		return textStyle;
+	}
+
+	private TextStyle solidityVersionTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(0, 0, 205));
+		return textStyle;
+	}
+
+	private TextStyle featureCallTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(6, 150, 14));
+		return textStyle;
+	}
+
 	@Override
 	public TextStyle keywordTextStyle() {
 		TextStyle textStyle = defaultTextStyle().copy();
-		textStyle.setColor(new RGB(14, 48, 131));
+		textStyle.setColor(new RGB(00, 00, 255));
 		textStyle.setStyle(SWT.BOLD);
+		return textStyle;
+	}
+	
+	@Override
+	public TextStyle commentTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(76, 136, 107));
+		textStyle.setStyle(SWT.ITALIC);
 		return textStyle;
 	}
 }
--- plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/highlighting/SoliditySemanticHighlighter.java
+++ plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/highlighting/SoliditySemanticHighlighter.java
@@ -16,13 +16,21 @@ package com.yakindu.solidity.ui.highlighting;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.nodemodel.BidiTreeIterator;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.yakindu.base.expressions.expressions.ElementReferenceExpression;
+import org.yakindu.base.expressions.expressions.FeatureCall;
 import org.yakindu.base.types.Declaration;
+import org.yakindu.base.types.Operation;
+import org.yakindu.base.types.Parameter;
+import org.yakindu.base.types.Property;
 
+import com.yakindu.solidity.solidity.VariableDefinition;
 
 /**
  * @author Andreas Muelder - Initial contribution and API
@@ -41,17 +49,78 @@ public class SoliditySemanticHighlighter
 			if (next.eIsProxy()) {
 				continue;
 			}
+			if (next instanceof Property) {
+				Property property = (Property) next;
+				provideHighligtingFor(property, acceptor);
+			}
+			if (next instanceof Operation) {
+				Operation operation = (Operation) next;
+				provideHighligtingFor(operation, acceptor);
+			}
 			if (next instanceof ElementReferenceExpression) {
-				if (next instanceof ElementReferenceExpression) {
-					ElementReferenceExpression expression = (ElementReferenceExpression) next;
-					provideHighligtingFor(expression, acceptor);
+				ElementReferenceExpression expression = (ElementReferenceExpression) next;
+				provideHighligtingFor(expression, acceptor);
+			}
+			if (next instanceof FeatureCall) {
+				FeatureCall featureCall = (FeatureCall) next;
+				provideHighligtingFor(featureCall, acceptor);
+			}
+		}
+	}
+
+	private void provideHighligtingFor(FeatureCall featureCall, IHighlightedPositionAcceptor acceptor) {
+		EObject feature = featureCall.getFeature();
+		if (feature instanceof Property) {
+			ICompositeNode node = NodeModelUtils.findActualNodeFor(featureCall);
+			if (node != null) {
+				acceptor.addPosition(node.getOffset(), node.getLength(),
+						SolidityHighlightingConfiguration.FEATURESCALL);
+			}
+		}
+	}
+
+	private void provideHighligtingFor(Operation definition, IHighlightedPositionAcceptor acceptor) {
+		provideHighligingForFunctionName(definition, acceptor);
+		provideHighligingForFunctionParameter(definition, acceptor);
+	}
+
+	private void provideHighligingForFunctionParameter(Operation definition, IHighlightedPositionAcceptor acceptor) {
+		for (Parameter parameter : definition.getParameters()) {
+			ICompositeNode node = NodeModelUtils.findActualNodeFor(parameter.getTypeSpecifier());
+			if (node != null) {
+				acceptor.addPosition(node.getOffset(), node.getLength() + 1, SolidityHighlightingConfiguration.TYPES);
+			}
+		}
+
+	}
+
+	private void provideHighligingForFunctionName(Operation definition, IHighlightedPositionAcceptor acceptor) {
+		ICompositeNode node = NodeModelUtils.findActualNodeFor(definition);
+		if (node != null && definition.getName() != null) {
+			BidiTreeIterator<INode> iterator = node.getAsTreeIterable().iterator();
+			while (iterator.hasNext()) {
+				INode nextNode = iterator.next();
+				if (definition.getName().equals(nextNode.getText())) {
+					acceptor.addPosition(nextNode.getOffset(), nextNode.getLength() + 1,
+							SolidityHighlightingConfiguration.FUNCTION_NAME);
+
 				}
+			}
+		}
+	}
+
+	private void provideHighligtingFor(Property property, IHighlightedPositionAcceptor acceptor) {
+		if (property instanceof VariableDefinition) {
+			ICompositeNode node = NodeModelUtils.findActualNodeFor(property.getTypeSpecifier());
+			if (node != null) {
+				acceptor.addPosition(node.getOffset(), node.getLength() + 1, SolidityHighlightingConfiguration.TYPES);
 			}
 		}
 	}
 
 	private void provideHighligtingFor(ElementReferenceExpression expression,
 			org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor acceptor) {
+
 		EObject reference = expression.getReference();
 		if (reference instanceof Declaration) {
 			Declaration decl = (Declaration) expression.getReference();
