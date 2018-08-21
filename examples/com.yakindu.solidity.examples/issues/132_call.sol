659dfe464cd415d2016960b22a0bb1bed36d2c1b
Florian Antony <florian.antony@itemis.de>
2018-08-21 14:40:56 +0200

    fix call arguments

---
+++ examples/com.yakindu.solidity.examples/issues/132_call.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.23;
+
+
+contract MyContract {
+    constructor () public{
+    }
+
+    function test() internal {
+        address a;
+        bool c1 = a.call("");
+        bytes b;
+        bool c2 = a.call(b);
+        bool b3 = a.call(b, "", 5);
+        bool b4 = a.call("", b, 5);
+    }
+}
