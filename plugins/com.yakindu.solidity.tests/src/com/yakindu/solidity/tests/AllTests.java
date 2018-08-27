3a127dda1cb026b7bc7bf7ef38ef52dbea0ef4a2
andreas muelder <muelder@itemis.de>
2018-08-27 17:57:22 +0200

    reworked test suite to work with Junit 4

---
+++ plugins/com.yakindu.solidity.tests/src/com/yakindu/solidity/tests/AllTests.java
@@ -0,0 +1,12 @@
+package com.yakindu.solidity.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ReferenceExamplesTest.class })
+
+public class AllTests {
+
+}
