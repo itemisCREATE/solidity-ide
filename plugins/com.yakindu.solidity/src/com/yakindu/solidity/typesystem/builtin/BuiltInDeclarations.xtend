6c222b2137ce8584386188c4749ff87d642e471f
Florian Antony <florian.antony@itemis.de>
2018-10-09 16:33:44 +0200

    fix build , tests & housekeeping

--- plugins/com.yakindu.solidity/src/com/yakindu/solidity/typesystem/builtin/BuiltInDeclarations.xtend
+++ plugins/com.yakindu.solidity/src/com/yakindu/solidity/typesystem/builtin/BuiltInDeclarations.xtend
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2018 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Andreas Muelder - Itemis AG - initial API and implementation
+ * 	Karsten Thoms   - Itemis AG - initial API and implementation
+ * 	Florian Antony  - Itemis AG - initial API and implementation
+ * 	committers of YAKINDU 
+ * 
+ */
 package com.yakindu.solidity.typesystem.builtin
 
 import com.yakindu.solidity.solidity.ContractDefinition
@@ -7,6 +21,10 @@ import org.eclipse.xtend.lib.annotations.Accessors
 import org.yakindu.base.types.Operation
 import org.yakindu.base.types.Property
 
+/**
+ * @author Florian Antony - initial contribution and API.
+ *
+ */
 @Accessors(PUBLIC_GETTER)
 abstract class BuiltInDeclarations {
 	
--- plugins/com.yakindu.solidity/src/com/yakindu/solidity/typesystem/builtin/SolidityBuiltInDeclarationsProvider.java
+++ plugins/com.yakindu.solidity/src/com/yakindu/solidity/typesystem/builtin/SolidityBuiltInDeclarationsProvider.java
@@ -1,5 +1,20 @@
+/**
+ * Copyright (c) 2018 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Andreas Muelder - Itemis AG - initial API and implementation
+ * 	Karsten Thoms   - Itemis AG - initial API and implementation
+ * 	Florian Antony  - Itemis AG - initial API and implementation
+ * 	committers of YAKINDU 
+ * 
+ */
 package com.yakindu.solidity.typesystem.builtin;
 
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
@@ -11,6 +26,12 @@ import com.google.inject.Singleton;
 import com.yakindu.solidity.solidity.PragmaSolidityDirective;
 import com.yakindu.solidity.solidity.SourceUnit;
 
+
+
+/**
+ * @author Florian Antony - initial contribution and API.
+ *
+ */
 @Singleton
 public class SolidityBuiltInDeclarationsProvider implements IBuiltInDeclarationsProvider {
 
@@ -38,10 +59,13 @@ public class SolidityBuiltInDeclarationsProvider implements IBuiltInDeclarations
 
 	private String calculatePragma(EObject element) {
 		SourceUnit sourceUnit = (SourceUnit) EcoreUtil2.getContainerOfType(element, SourceUnit.class);
-		PragmaSolidityDirective pragmaDirective = EcoreUtil2
-				.getAllContentsOfType(sourceUnit, PragmaSolidityDirective.class).get(0);
-		if (pragmaDirective != null) {
-			return pragmaDirective.getVersion();
+		List<PragmaSolidityDirective> pragmaDirectives = EcoreUtil2.getAllContentsOfType(sourceUnit,
+				PragmaSolidityDirective.class);
+		if (!pragmaDirectives.isEmpty()) {
+			PragmaSolidityDirective pragmaDirective = pragmaDirectives.get(0);
+			if (pragmaDirective != null) {
+				return pragmaDirective.getVersion();
+			}
 		}
 		return SolidityVersions.DEFAULT_VERSION;
 	}
