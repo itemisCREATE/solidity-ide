f8c83b01cceb5f291dfefb5c9068be675ec2ab28
jthoene <jonathan@fam-thoene.de>
2018-10-16 15:35:13 +0200

    Implementation of ProposalProvider and Hyperlink for ImportDirectives

--- plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/editor/SolidityHyperlinkHelper.java
+++ plugins/com.yakindu.solidity.ui/src/com/yakindu/solidity/ui/editor/SolidityHyperlinkHelper.java
@@ -14,15 +14,24 @@
  */
 package com.yakindu.solidity.ui.editor;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.Region;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.hyperlinking.HyperlinkHelper;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
+import org.eclipse.xtext.ui.editor.hyperlinking.XtextHyperlink;
 import org.yakindu.base.types.typesystem.AbstractTypeSystem;
 import org.yakindu.base.types.typesystem.ITypeSystem;
+
+import com.yakindu.solidity.solidity.ImportDirective;
+import com.yakindu.solidity.solidity.SolidityPackage;
 
 /**
  * Hyperlinking customizations:
@@ -30,6 +39,7 @@ import org.yakindu.base.types.typesystem.ITypeSystem;
  * <li>Disable hyperlinks for build-in elements
  * 
  * @author Karsten Thoms - Initial contribution and API 
+ * @author Jonathan Thoene
  *
  */
 public class SolidityHyperlinkHelper extends HyperlinkHelper {
@@ -43,5 +53,45 @@ public class SolidityHyperlinkHelper extends HyperlinkHelper {
 			return;
 		}
 		super.createHyperlinksTo(from, region, target, acceptor);
+	}
+	
+	@Override
+	public void createHyperlinksByOffset(XtextResource resource, int offset, IHyperlinkAcceptor acceptor) {
+		createImportedNamespacesHyperlinksByOffset(resource, offset, acceptor);
+		super.createHyperlinksByOffset(resource, offset, acceptor);
+	}
+
+	protected void createImportedNamespacesHyperlinksByOffset(XtextResource resource, int offset,
+			IHyperlinkAcceptor acceptor) {
+		INode node = NodeModelUtils.findLeafNodeAtOffset(resource.getParseResult().getRootNode(), offset);
+		if (node != null) {			
+			List<INode> importNodes = NodeModelUtils.findNodesForFeature(node.getSemanticElement(), SolidityPackage.Literals.IMPORT_DIRECTIVE__IMPORTED_NAMESPACE);
+			if (importNodes != null && !importNodes.isEmpty()) {
+				for (INode importNode : importNodes) {
+					ImportDirective importElement = (ImportDirective) importNode.getSemanticElement();
+					URI targetURI = getFileURIOfImport(importElement);
+					XtextHyperlink result = getHyperlinkProvider().get();
+					result.setURI(targetURI);
+					Region region = new Region(importNode.getOffset(), importNode.getLength());
+					result.setHyperlinkRegion(region);
+					result.setHyperlinkText(targetURI.toString());
+					acceptor.accept(result);
+				}
+			}
+		}
+	}
+	
+	protected URI getFileURIOfImport (ImportDirective importElement) {
+		URI fileURI = importElement.eResource().getURI().trimSegments(1);
+		String importURI = importElement.getImportedNamespace();
+		while (importURI.startsWith("../")) {
+			fileURI = fileURI.trimSegments(1);
+			importURI = importURI.replaceFirst("../", "");
+		}
+		String[] segments = importURI.split("/");
+		for (String segment : segments) {
+			fileURI = fileURI.appendSegment(segment);
+		}
+		return fileURI;
 	}
 }
