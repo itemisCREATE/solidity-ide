5ace96049a6df24468ceac0439d556ce58de7b72
amuel <a.muelder@gmail.com>
2018-10-27 22:53:12 +0200

    fixed comment, removed disposable command

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -8,7 +8,7 @@ import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
-    // The server is a locally installed in src/mydsl
+    // The server is a locally installed in src/ls
     let launcher = os.platform() === 'win32' ? 'solidity-ls.bat' : 'solidity-ls';
     let script = context.asAbsolutePath(path.join('src', 'ls', 'bin', launcher));
     
@@ -26,19 +26,7 @@ export function activate(context: ExtensionContext) {
     
     // Create the language client and start the client.
     let lc = new LanguageClient('Solidity Language Server', serverOptions, clientOptions);
-    
-    var disposable2 =commands.registerCommand("sol.a.proxy", async () => {
-        let activeEditor = window.activeTextEditor;
-        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'sol') {
-            return;
-        }
-
-        if (activeEditor.document.uri instanceof Uri) {
-            commands.executeCommand("sol.a", activeEditor.document.uri.toString());
-        }
-    })
-    context.subscriptions.push(disposable2);
-    
+      
     // enable tracing (.Off, .Messages, Verbose)
     lc.trace = Trace.Verbose;
     let disposable = lc.start();
