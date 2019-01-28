ab4cab05dae9f048eba6e5bae7c707a71bc7570a
Fabio Spiekermann <fspiekermann@itemis.de>
2019-01-28 10:57:57 +0100

    Update hello_world.sol

--- extensions/theia/code/hello_world.sol
+++ extensions/theia/code/hello_world.sol
@@ -1,27 +1,20 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity ^0.4.23;
 
-contract Mortal {
-    /* Define variable owner of the type address */
-    address owner;
-
-    /* This constructor is executed at initialization and sets the owner of the contract */
-    constructor() public { owner = msg.sender; }
-
-    /* Function to recover the funds on the contract */
-    function kill() public { if (msg.sender == owner) selfdestruct(msg.sender); }
-}
-
-contract Greeter is Mortal {
-    /* Define variable greeting of the type string */
-    string greeting;
-
-    /* This runs when the contract is executed */
-    constructor(string memory _greeting) public {
-        greeting = _greeting;
+contract MyContract {
+    address creator;
+    
+    function MyContract () {
+        creator = msg.sender;
     }
-
-    /* Main function */
-    function greet() public view returns (string memory) {
-        return greeting;
+    
+    // TODO Add functions
+    
+    /**********
+        Standard kill() function to recover funds
+        **********/
+    function kill() {
+        if (msg.sender == creator) {
+            suicide(creator); // kills this contract and sends remaining funds back to creator
+        }
     }
 }
