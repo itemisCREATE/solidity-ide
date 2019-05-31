ba0082a126c9fd7f631ce24ec8176b384c67413a
andreas muelder <muelder@itemis.de>
2019-05-31 14:37:20 +0200

    fixed tuple expressions

---
+++ examples/com.yakindu.solidity.examples/issues/230_tuple_types.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.4 ;
+
+contract C {
+    uint[] data;
+
+    function f() public pure returns (uint, bool, uint) {
+        return (7, true, 2);
+    }
+
+    function g() public {
+        // Variables declared with type and assigned from the returned tuple,
+        // not all elements have to be specified (but the number must match).
+        (uint x, , uint y) = f(); 
+        // Common trick to swap values -- does not work for non-value storage types.
+        (x, y) = (y, x);
+        // Components can be left out (also for variable declarations).
+        (data.length, , ) = f(); // Sets the length to 7
+    }
+}
