169c70785beef019431d06db20372adb6ef39715
anyssen <nyssen@itemis.com>
2019-08-02 11:27:54 +0200

    Cleanup theia extension.

--- extensions/theia/app/webpack.config.js
+++ extensions/theia/app/webpack.config.js
@@ -13,9 +13,9 @@ const { mode }  = yargs.option('mode', {
 }).argv;
 const development = mode === 'development';
 
-const monacoEditorCorePath = development ? '/mnt/c/programmieren/yakindu/solidity-ide/extensions/theia/ide/node_modules/@typefox/monaco-editor-core/dev/vs' : '/mnt/c/programmieren/yakindu/solidity-ide/extensions/theia/ide/node_modules/@typefox/monaco-editor-core/min/vs';
-const monacoCssLanguagePath = '/mnt/c/programmieren/yakindu/solidity-ide/extensions/theia/ide/node_modules/monaco-css/release/min';
-const monacoHtmlLanguagePath = '/mnt/c/programmieren/yakindu/solidity-ide/extensions/theia/ide/node_modules/monaco-html/release/min';
+const monacoEditorCorePath = development ? '/Users/nyssen/git/solidity-ide/extensions/theia/node_modules/@typefox/monaco-editor-core/dev/vs' : '/Users/nyssen/git/solidity-ide/extensions/theia/node_modules/@typefox/monaco-editor-core/min/vs';
+const monacoCssLanguagePath = '/Users/nyssen/git/solidity-ide/extensions/theia/node_modules/monaco-css/release/min';
+const monacoHtmlLanguagePath = '/Users/nyssen/git/solidity-ide/extensions/theia/node_modules/monaco-html/release/min';
 
 module.exports = {
     entry: path.resolve(__dirname, 'src-gen/frontend/index.js'),
@@ -66,7 +66,7 @@ module.exports = {
                 test: /\.(jpg|png|gif)$/,
                 loader: 'file-loader',
                 options: {
-                    name: '[path][name].[hash].[ext]',
+                    name: '[hash].[ext]',
                 }
             },
             {
