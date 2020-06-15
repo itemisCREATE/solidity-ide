3719c96aef946a4424cf81ed676d0d4f7b8112cd
Jonathan Thoene <jonathan@fam-thoene.de>
2020-06-15 17:16:43 +0200

    Issue 325
    This fixes #325
    Received and fallback function can now be defined.

---
+++ examples/com.yakindu.solidity.examples/issues/325_fallback_function.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=^0.6.2 <^0.7.0;
+
+contract Test {
+    // This function is called for all messages sent to
+    // this contract (there is no other function).
+    // Sending Ether to this contract will cause an exception,
+    // because the fallback function does not have the `payable`
+    // modifier.
+    fallback() external { x = 1; }
+    uint x;
+}
