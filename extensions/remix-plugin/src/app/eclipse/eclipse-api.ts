d5d6245b91a8ca5fedbf75b2211a4cdf21bebf2e
Florian Antony <flow.antony@googlemail.com>
2020-07-27 09:58:52 +0200

    add interface to communicate with eclipse

---
+++ extensions/remix-plugin/src/app/eclipse/eclipse-api.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Message } from '@remixproject/engine';
+
+export interface EclipseIDE extends Window {
+  // Ask Eclipse to create a Browser instance. Return the id of the browser instance
+  eclipse_connect: (profile: string) => string;
+
+  // Ask Eclipse to destroy a Browser instance
+  eclipse_disconnect: (id: string) => boolean;
+
+  // Send a message to Eclipse
+  eclipse_send: (message: string) => void;
+}
+
+function getWindow(): any {
+  return window;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EclipseService {
+
+  constructor() { }
+
+  get nativeEclipse(): EclipseIDE {
+    return getWindow();
+  }
+}
